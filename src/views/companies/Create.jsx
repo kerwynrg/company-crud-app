aa7eb1e13dac97bf96833fddfab5ac224c81c7a7
Kerwyn Rojas <kerwyn.rg@gmail.com>
2016-02-23 17:54:09 -0700

    Added formsy

--- src/views/companies/Create.jsx
+++ src/views/companies/Create.jsx
@@ -1,11 +1,29 @@
 /* @flow */
 import { BaseComponent } from 'components/core';
+import { FormsyText } from 'formsy-material-ui';
+import { Form } from 'formsy-react';
 
 class CreateView extends BaseComponent {
   render () {
     return (
       <div className='container text-center'>
         <h1>New company</h1>
+        <Form>
+          <FormsyText
+            name='name'
+            validations='isWords'
+            validationError='Error name'
+            required
+            hintText='Indicates name'
+            floatingLabelText='Name' />
+          <FormsyText
+            name='address'
+            validations='isWords'
+            validationError='Error address'
+            required
+            hintText='Indicates address'
+            floatingLabelText='Address' />
+        </Form>
       </div>
     );
   }
